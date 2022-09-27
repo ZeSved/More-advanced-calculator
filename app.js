08dace1b6159bd8c4aa64bbe234e77429e0675d5
ZeSved <timothy.korell@gmail.com>
2022-09-27 21:02:40 +0200

    started it

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const multiply = document.querySelector('.multiply');
+const divide = document.querySelector('.minus');
+const negate = document.querySelector('.negate');
+const equal = document.querySelector('.equal');
+const result = document.querySelector('.result');
+const dot = document.querySelector('.dot');
+dot?.addEventListener('click', () => {
+    console.log('works');
+});
